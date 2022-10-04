a575c735b1a0f053dd0d960fdb7e04f7a9ed1b8d
Victor-Brezoev <victor.brezoev@gmail.com>
2022-10-04 19:53:13 +0300

    changes

--- sections/Main/Main.jsx
+++ sections/Main/Main.jsx
@@ -12,28 +12,49 @@ import {
   Cards,
 } from './elements';
 
-const cardProps1 = {
-  //   image: { src: '/img/heroimg.png', alt: '', width: 10, height: 24 },
-  title: 'Brief',
-  description:
-    'Complete brief writing or simply guidance on what to include, we have got you covered.',
-};
+const cardProps = [
+  {
+    //   image: { src: '/img/heroimg.png', alt: '', width: 10, height: 24 },
+    title: 'Brief',
+    description:
+      'Complete brief writing or simply guidance on what to include, we have got you covered.'
+        .split('brief writing or')
+        .map((description, index) => (
+          <>
+            {index !== 0 && <b>brief writing or simply guidance</b>}
+            {description}
+          </>
+        )),
+  },
+  {
+    //   image: { src: '/img/heroimg.png', alt: '', width: 1000, height: 1680 },
+    title: 'Search',
+    description:
+      'In-debth agency search covering; criteria matching, door knocking and due-dilligenc evetting.'
+        .split('criteria matching')
+        .map((description, index) => (
+          <>
+            {index !== 0 && <b>criteria matching</b>}
+            {description}
+          </>
+        )),
+  },
+  {
+    //   image: { src: '/img/heroimg.png', alt: '', width: 1000, height: 1680 },
+    title: 'Pitch',
+    description:
+      'Comprehensive pitch management, including comms, diary management and pitch hosting.'
+        .split('pitch management')
+        .map((description, index) => (
+          <>
+            {index !== 0 && <b>pitch management</b>}
+            {description}
+          </>
+        )),
+  },
+];
 
-const cardProps2 = {
-  //   image: { src: '/img/heroimg.png', alt: '', width: 1000, height: 1680 },
-  title: 'Search',
-  description:
-    'In-debth agency search covering; criteria matching, door knocking and due-dilligenc evetting.',
-};
-
-const cardProps3 = {
-  //   image: { src: '/img/heroimg.png', alt: '', width: 1000, height: 1680 },
-  title: 'Pitch',
-  description:
-    'Comprehensive pitch management, including comms, diary management and pitch hosting.',
-};
-
-export const Main = ({ image, title, description, ...props }) => {
+export const Main = ({ image, title, description }) => {
   return (
     <WrapperMain>
       <ContainerMain>
@@ -52,9 +73,15 @@ export const Main = ({ image, title, description, ...props }) => {
             />
           </StyledImageContainer>
           <Cards>
-            <Card {...cardProps1}></Card>
-            <Card {...cardProps2}></Card>
-            <Card {...cardProps3}></Card>
+            {cardProps.map((element, index) => (
+              <Card
+                key={index}
+                title={element.title}
+                image={element.image}
+                description={element.description}
+                order={index + 1}
+              />
+            ))}
           </Cards>
         </InnerMain>
       </ContainerMain>
